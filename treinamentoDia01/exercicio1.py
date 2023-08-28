095860662b28b89bcaf135b6c9fa0aa98597e34f
jnicklr <jnichollaslr@gmail.com>
2023-08-28 16:44:04 -0300

    dia 1 - primeiro exercício python

---
+++ treinamentoDia01/exercicio1.py
@@ -0,0 +1,7 @@
+# armazena o valor numérico escolhido em uma variável!
+numero = int(input("Digite um número: "))
+
+# imprime os valores de incremento e decremento direto na formatação do print!
+print(
+    f"O antecessor do número {numero} é {numero-1}.\nO sucessor do número {numero} é {numero+1}."
+)
