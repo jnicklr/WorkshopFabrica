645feae1210065e4bf1a337350b55b3b7c6f19c6
jnicklr <jnichollaslr@gmail.com>
2023-09-01 19:35:08 -0300

    models professor

--- apps/professor/models.py
+++ apps/professor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=200)
+    area_atuacao = models.CharField(max_length=200, verbose_name='Area de Atuação')
+    valor_hora = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
