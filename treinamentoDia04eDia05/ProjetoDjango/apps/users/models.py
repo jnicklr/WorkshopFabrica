3480a9f5e6ca28e32fa43ea9e8b9aed42439f108
jnicklr <jnichollaslr@gmail.com>
2023-09-01 15:44:45 -0300

    dia 5 - relacionamento 1-N entre Users e Tasks, Registro no Admin

--- treinamentoDia04eDia05/ProjetoDjango/apps/users/models.py
+++ treinamentoDia04eDia05/ProjetoDjango/apps/users/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
-    
+    email = models.EmailField()
+
     def __str__(self):
         return self.name
